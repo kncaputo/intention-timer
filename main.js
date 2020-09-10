ba9ed99d8fc05576dc443728706e27f462406bf0
Peyton Jones <peytonj.dev@gmail.com>
2020-09-10 17:00:42 -0600

    Add functionality to buttons and inputs

--- main.js
+++ main.js
@@ -7,14 +7,53 @@ var meditateGraphic = document.querySelector("#meditate-graphic");
 var activeMeditate = document.querySelector("#active-meditate");
 var exerciseGraphic = document.querySelector("#exercise-graphic");
 var activeExercise = document.querySelector("#active-exercise");
+var startActivityBtn = document.querySelector('.start-activity-btn')
+var inputBox = document.querySelector(".time-input");
+var categoryBtn = document.querySelector('.category-btns')
+var goal = document.querySelector('.goal')
+var min = document.querySelector('.min')
+var sec = document.querySelector('.sec')
+// var choice = document.querySelector('.user-choice')
 
+
+//Global Variables 
+var invalidChars = [
+    "-",
+    "+",
+    "e",
+  ];
+
+var currentActivity = new Activity 
+
+
+//Event Listeners
 studyButton.addEventListener('click', selectStudy);
 
 meditateButton.addEventListener('click', selectMeditate);
 
 exerciseButton.addEventListener('click', selectExercise);
 
+startActivityBtn.addEventListener('click', startActivity)
 
+inputBox.addEventListener("keydown", function startActivity(event){
+    if (invalidChars.includes(event.key)) {
+      event.preventDefault();
+    }
+})
+
+
+categoryBtn.addEventListener('click', function(event) {
+    if (event.target.className === 'study-btn') {
+      this.category = "Study";
+      console.log(this.category)
+    }
+    if (event.target.className === 'meditate-btn') {
+      this.category = "Meditate";
+    }
+    if (event.target.className === 'exercise-btn') {
+      this.category = "Exercise";
+    }
+})
 
 // TO-DO Potential refactoring
 
@@ -35,3 +74,16 @@ function selectExercise() {
   exerciseGraphic.classList.toggle("hidden");
   activeExercise.classList.toggle("hidden");
 }
+
+
+function startActivity(){
+  if (isNaN(min.value) === true || isNaN(sec.value) === true){
+    this.minutes = min.value
+    this.seconds = sec.value
+  }else if(goal.value !== undefined){
+      this.description = goal.value
+      console.log(this.description)
+  }else if(this.category !== undefined){
+      console.log(currentActivity)
+  }
+ }
